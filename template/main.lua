1b7b21b7c039bbed290935a60b1dba67653d93c5
Dragon20C <dragon212c@gmail.com>
2026-06-14 14:46:07 +0100

    reorganised folder dirs and refactored cli.odin.

--- template/main.lua
+++ template/main.lua
@@ -1,8 +1,8 @@
 function _config()
 	---@type Cookie.Config
 	return {
-		game_title = "my game",
-		game_id = "GameID",
+		game_title = "Cookie Game",
+		game_id = "Dragon20C.CookieGame",
 		width = 640,
 		height = 360,
 		scale = 1
@@ -10,26 +10,10 @@ function _config()
 end
 
 function _init()
-	print("Init started!")
-	print("platform =", cookie.PLATFORM)
-	if cookie.IS_DEV then
-		print("Cookie is dev is on!")
-	else
-		print("Cookie is dev is off!")
-	end
 end
 
 function _update(dt)
-	--print("Update: elapsed =", cookie.elapsed)
 end
 
 function _draw(dt)
-	local selected_color = gfx.COLOR_DARK_RED
-
-	if cookie.IS_DEV then
-		selected_color = gfx.COLOR_MINT
-	end
-	gfx.rect(0, 0, cookie.Width, cookie.Height, selected_color)
-	gfx.clear(selected_color)
-	gfx.rect(100, 50, 32, 32, gfx.COLOR_PINK)
 end
